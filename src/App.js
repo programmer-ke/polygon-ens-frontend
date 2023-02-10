a0d8fedd2a5c01af81ba48fd18e9c874250213d8
Kenny Rachuonyo <krmboya@gmail.com>
2023-02-10 12:32:01 +0300

    enable user connect their wallet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState }  from 'react';
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -8,31 +8,86 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
 
-  return (
-		<div className="App">
-			<div className="container">
+    const [currentAccount, setCurrentAccount] = useState("");
 
-				<div className="header-container">
-					<header>
-            <div className="left">
-              <p className="title">🐱‍👤 Alfa Name Service</p>
-              <p className="subtitle">Your immortal API on the blockchain!</p>
-            </div>
-					</header>
-				</div>
+    const renderNotConnectedContainer = () => (
+	<div className="connect-wallet-container">
+	    <img src="https://media.giphy.com/media/3b6rU7cb6g8zykC5uN/giphy.gif" alt="gif"/>
+	    <button onClick={connectWallet} className="cta-button connect-wallet-button">
+		Connect Wallet
+	    </button>
+	</div>
+    );
 
-        <div className="footer-container">
-					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-					<a
-						className="footer-text"
-						href={TWITTER_LINK}
-						target="_blank"
-						rel="noreferrer"
-					>{`built with @${TWITTER_HANDLE}`}</a>
-				</div>
+    const connectWallet = async () => {
+	try {
+	    const { ethereum } = window;
+	    if (!ethereum) {
+		alert("Please install a wallet (metamask)");
+		return;
+	    }
+
+	    // request access to wallet account
+	    const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+	    console.log("Connected account", accounts[0]);
+	    setCurrentAccount(accounts[0]);
+
+	} catch (error) {
+	    console.log(error);
+	}
+    };
+
+    const checkIfWalletIsConnected = async () => {
+	const { ethereum } = window;
+	if (!ethereum) {
+	    console.log("Make sure you have metamask");
+	} else {
+	    console.log("We have the ethereum object");
+	}
+
+	// Get any authorized accounts
+	const accounts = await ethereum.request({ method: "eth_accounts" });
+	if (accounts.length > 0) {
+	    const account = accounts[0];
+	    console.log("Found authorized account:", account);
+	} else {
+	    console.log("No authorized account found");
+	}
+
+    };
+
+    useEffect(() => {
+	checkIfWalletIsConnected();
+    }, []);
+
+    return (
+	<div className="App">
+	    <div className="container">
+
+		<div className="header-container">
+		    <header>
+			<div className="left">
+			    <p className="title">🐱‍👤 Alfa Name Service</p>
+			    <p className="subtitle">Your immortal API on the blockchain!</p>
 			</div>
+		    </header>
 		</div>
-	);
+
+		{/* Render method */}
+		{!currentAccount && renderNotConnectedContainer()}
+
+		<div className="footer-container">
+		    <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+		    <a
+			className="footer-text"
+			href={TWITTER_LINK}
+			target="_blank"
+			rel="noreferrer"
+		    >{`built with @${TWITTER_HANDLE}`}</a>
+		</div>
+	    </div>
+	</div>
+    );
 }
 
 export default App;
